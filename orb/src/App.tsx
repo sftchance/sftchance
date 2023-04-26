e57e14771a5fcd2e22add04f027bfdc85853142b
sftchance <chance+sft@utc24.io>
2023-04-25 20:29:40 -0500

    feat: frontend foundation

--- orb/src/App.tsx
+++ orb/src/App.tsx
@@ -2,19 +2,115 @@ import { useState } from 'react';
 
 import { Color } from './types';
 
+import orbNoise from './assets/orb-noise.png';
+import orbTexture from './assets/orb-texture.png';
+
 import './App.css';
 
+const DEFAULT_COLORS: Color[] = [
+    {
+        hex: '#7d67bf',
+        position: 0,
+    },
+    {
+        hex: '#9c81ee',
+        position: 10,
+    },
+    {
+        hex: '#8c6fde',
+        position: 20.95,
+    },
+    {
+        hex: '#52308a',
+        position: 70.96,
+    },
+    {
+        hex: '#52308a',
+        position: 83.61,
+    },
+    {
+        hex: '#31054c',
+        position: 93.72,
+    },
+    {
+        hex: '#1a0139',
+        position: 100,
+    },
+];
+
+const getRandomColors = (colors: Color[]): Color[] => {
+    return colors.map((color) => {
+        if (color.locked) {
+            return color;
+        }
+
+        return {
+            ...color,
+            hex: `#${Math.floor(Math.random() * 16777215)
+                .toString(16)
+                .padStart(6, '0')}`,
+        };
+    });
+};
+
 function App() {
-    const [colors, setColors] = useState<Color[]>([]);
+    const [colors, setColors] = useState<Color[]>(DEFAULT_COLORS);
+
+    const orbStyle = {
+        background: `radial-gradient(95.24% 100.5% at 2.04% 40.07%, ${colors
+            .map((color) => {
+                return `${color.hex} ${color.position}%`;
+            })
+            .join(', ')})`,
+    };
 
     return (
         <>
-            <h1>Test</h1>
+            <div className="preview">
+                <div className="image">
+                    <div className="orb blurred" style={orbStyle}></div>
+                    <img className="texture blurred" src={orbTexture} alt="texture" />
+                    <img className="noise blurred" src={orbNoise} alt="noise" />
+                    <div className="blur"></div>
+                    <div className="orb" style={orbStyle}></div>
+                    <img className="texture" src={orbTexture} alt="texture" />
+                    <img className="noise" src={orbNoise} alt="noise" />
+                    <div className="shadow"></div>
+                </div>
+            </div>
 
-            <input></input>
+            <button
+                onClick={() => {
+                    setColors((colors) => getRandomColors(colors));
+                }}
+            >
+                Randomize
+            </button>
 
-            {colors.map((color) => {
-                return <p key={color.hex}>{color.hex}</p>;
+            {colors.map((color, index) => {
+                return (
+                    <div key={index} className="color">
+                        <input
+                            type="color"
+                            value={color.hex}
+                            onChange={(event) => {
+                                const newColors = [...colors];
+                                newColors[index].hex = event.target.value as `#${string}`;
+                                setColors(newColors);
+                            }}
+                        />
+
+                        <button
+                            onClick={() => {
+                                const newColors = [...colors];
+                                newColors[index].locked = !newColors[index].locked;
+                                setColors(newColors);
+                            }}
+                        >
+                            {color.locked ? '🔒' : '🔓'}
+                        </button>
+                    </div>
+                );
             })}
         </>
     );
--- orb/src/types/Color.tsx
+++ orb/src/types/Color.tsx
@@ -1,5 +1,7 @@
 interface Color {
     hex: `#${string}`;
+    position: number;
+    locked?: boolean | undefined;
 }
 
 export type { Color };
